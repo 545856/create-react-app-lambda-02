dd9d30f8b81247c488ec231a1a0b27ed4d7d109d
545856 <35502381+545856@users.noreply.github.com>
2021-05-06 20:31:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,24 @@ class LambdaDemo extends Component {
 
   }
   
+  const YoutubeEmbed = ({ embedId }) => (
+  <div className="video-responsive">
+    <iframe
+      width="853"
+      height="480"
+      src={`https://www.youtube.com/embed/${embedId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    />
+  </div>
+);
+
+YoutubeEmbed.propTypes = {
+  embedId: PropTypes.string.isRequired
+};
+  
   componentDidMount() {
 		const queryString = window.location.search;
 		console.log(queryString);
