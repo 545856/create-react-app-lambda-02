53d660167eef099b18e6cfe63bad0ff4801d6332
545856 <35502381+545856@users.noreply.github.com>
2021-05-06 21:31:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ class LambdaDemo extends Component {
   componentDidMount() {
 		const queryString = window.location.search;
 		console.log(queryString);
-		if (queryString === "monet") {
+		value = queryString.split('=')
+		if (value[1] === "monet") {
 			document.getElementById("monetFr").classList.remove('hiddenVid');
 			document.getElementById("monetEn").classList.remove('hiddenVid');
 		}
