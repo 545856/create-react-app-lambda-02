47b34352906cf6bde13bf816a1b16c8791802182
545856 <35502381+545856@users.noreply.github.com>
2021-05-07 08:51:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -58,6 +58,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+	  <br/>
           <LambdaDemo />
         </header>
       </div>
